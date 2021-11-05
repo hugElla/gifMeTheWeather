baa497ae725af346b463d65b62097b7034cb847d
HugoArriojas <hugo.arriojas@hotmail.com>
2021-11-05 14:26:15 -0400

    added javascript for API calls to Giphy and Accuweather

--- scripts.js
+++ scripts.js
@@ -1,21 +1,38 @@
 const app = {}
 
 app.getWeather = () => {
-  fetch('http://dataservice.accuweather.com/forecasts/v1/daily/5day/55488?apikey=vGXkpHg0aMsvhNmAxwDASbd4qs7nQ8tQ&language=en-us&details=true&metric=true')
-      .then(function (response) {
-          if (response.status === 200) {
-              return response.json();
-          } else {
-              console.log("Error")
-          }
-      })
-      .then((data) => {
-          console.log(data)
-      })
+    fetch('http://dataservice.accuweather.com/forecasts/v1/daily/5day/55488?apikey=vGXkpHg0aMsvhNmAxwDASbd4qs7nQ8tQ&language=en-us&details=true&metric=true')
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                console.log("Error")
+            }
+        })
+        .then((data) => {
+            console.log(data.DailyForecasts);
+        })
 }
 
+app.giphyApiKey = "bT4cKp5t3W32Z0y3nvvnDyW6I3neAsH2"
+app.weather = "cloudy"
+app.retrieveGif = () => {
+    fetch('https://api.giphy.com/v1/gifs/search?q=${app.weather}&api_key=bT4cKp5t3W32Z0y3nvvnDyW6I3neAsH2')
+        .then(function (response) {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                console.log("Error")
+            }
+        })
+        .then((data) => {
+            console.log(data.data);
+        })
+}  
+
 app.init = () => {
-  app.getWeather()
+app.getWeather();
+app.retrieveGif();
 }
 
 app.init()
