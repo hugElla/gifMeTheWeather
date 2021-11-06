a89704e6ec56364621b84abb474d2b0237a24a46
HugoArriojas <hugo.arriojas@hotmail.com>
2021-11-06 14:29:23 -0400

    brought chosen GIF images into an array to be displayed

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const app = {}
 
 app.randomizer = (array) => {
-            const randomIndex = Math.floor(Math.random() * 10);
+            const randomIndex = Math.floor(Math.random() * 8);
             return array[randomIndex];
         }
 
 app.location = 55488;
-// app.apiKey = "vGXkpHg0aMsvhNmAxwDASbd4qs7nQ8tQ";
-app.apiKey = "F3qBixSACB4wgorFTTxE3ANdJkzcjhtA";
+app.apiKey = "vGXkpHg0aMsvhNmAxwDASbd4qs7nQ8tQ";
+// app.apiKey = "F3qBixSACB4wgorFTTxE3ANdJkzcjhtA";
 // app.apiKey = "fwFkFHtNtvIEuQyNesPT4F1Watb33kP3";
 app.url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${app.location}?apikey=${app.apiKey}&language=en-us&details=true&metric=true`
 
@@ -17,7 +17,7 @@ app.getWeather = () => {
             return response.json();                
         })
         .then((data) => {
-            console.log(data.DailyForecasts);
+            // console.log(data.DailyForecasts);
             app.displayForecast(data.DailyForecasts);
         })
 }
@@ -64,21 +64,24 @@ app.displayForecast = (arrayFromWeather) => {
             dayListEl.appendChild(dayElements);
         } 
 
+        
+
 
 
 
         
     
-
+        // send the weather forecast for the day to the GIPHY API
         app.retrieveGif(dayPhrase);
         
     })    
 }
 
+app.chosenGifs = [];
 
 app.giphyApiKey = "bT4cKp5t3W32Z0y3nvvnDyW6I3neAsH2"
 app.retrieveGif = (iconPhrase) => {
-    fetch(`https://api.giphy.com/v1/gifs/search?q=weather+`+iconPhrase+`&api_key=${app.giphyApiKey}`)
+    fetch(`https://api.giphy.com/v1/gifs/search?q=weather+sky+`+iconPhrase+`&api_key=${app.giphyApiKey}`)
         .then(function (response) {
             return response.json();
         })
@@ -86,28 +89,33 @@ app.retrieveGif = (iconPhrase) => {
             const obtainedGifs = gifArray.data
 
             // Querying the DOM for the GIF container
-            const gifContainer = document.querySelector(".gifArea");
+            const gifContainers = document.querySelectorAll(".gifArea");
+            app.chosenGifs.push(app.randomizer(obtainedGifs));
+        
             
-            const chosenGifs = [app.randomizer(obtainedGifs)]
-            console.log(chosenGifs)
-
-            chosenGifs.forEach((gif) =>{
-                // an create image element
-                const imgEl = document.createElement("img");
-
-                // add the content to the img element src & alt text
-                imgEl.src = gif.images.original.url;
-                imgEl.alt = gif.title;
-
-                // console.log(imgEl)
-
-                gifContainer.appendChild(imgEl);
         })
-        })
-}  
+    }  
+
+for (i = 0; i < app.chosenGifs.length; i++){
+    for(i = 0; i<)
 
 
+}
 
+    
+app.chosenGifs.forEach((gif) =>{
+    // an create image element
+    const imgEl = document.createElement("img");
+    // console.log(gif)
+    // add the content to the img element src & alt text
+    imgEl.src = gif.images.original.url;
+    imgEl.alt = gif.title;
+
+    console.log(imgEl)
+    
+})
+    
+    
 app.init = () => {
     app.getWeather();
 }
