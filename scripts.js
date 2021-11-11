d7d318102526a3f6decf343bcbf7b097e12fc419
HugoArriojas <hugo.arriojas@hotmail.com>
2021-11-10 19:12:56 -0500

    changed temperature divs in HTML so that the High and Low are above the temperature values

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 // creating a namespace object
 const app = {};
 
-// this is a comment
-
 // randomizer function for picking a random gif
 app.randomizer = (array) => {
   const randomIndex = Math.floor(Math.random() * 7);
@@ -41,6 +39,9 @@ app.locationSubmission = () => {
 
   });
 };
+
+
+
 
 // the same as locationSubmission() but for the button on the splash page. shows weather, hides splash page
 app.showHide = () => {
@@ -112,8 +113,6 @@ app.getLocation = (locationQuery) => {
 
 
 
-
-
 // api call to get weather for inputted location
 app.getWeather = (location) => {
 
@@ -140,7 +139,8 @@ app.getWeather = (location) => {
 
 //Weekday name array
   // goes to Wednesday again as it iterates +5 to the array, on Saturday going to array[12]
-  app.weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"]
+app.weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"]
+
 
 
 // Empty arrays for daily objects
@@ -239,17 +239,17 @@ app.displayForecast = (arrayFromWeather) => {
 
     app.nightWindArray.push(dayWeather.Night.Wind.Speed.Value + dayWeather.Day.Wind.Speed.Unit);
 
-    app.realTempMax.push(Math.round(dayWeather.Temperature.Maximum.Value) + "°" + dayWeather.Temperature.Maximum.Unit);
-    app.realTempMin.push(Math.round(dayWeather.Temperature.Minimum.Value) + "°" + dayWeather.Temperature.Minimum.Unit);
-    app.feelsLikeMax.push(Math.round(dayWeather.RealFeelTemperature.Maximum.Value) + "°" + dayWeather.RealFeelTemperature.Maximum.Unit);
-    app.feelsLikeMin.push(Math.round(dayWeather.RealFeelTemperature.Minimum.Value) + "°" + dayWeather.RealFeelTemperature.Minimum.Unit);
-
+    app.realTempMax.push(Math.round(dayWeather.Temperature.Maximum.Value) + "° " + dayWeather.Temperature.Maximum.Unit);
+    app.realTempMin.push(Math.round(dayWeather.Temperature.Minimum.Value) + "° " + dayWeather.Temperature.Minimum.Unit);
+    app.feelsLikeMax.push(Math.round(dayWeather.RealFeelTemperature.Maximum.Value) + "° " + dayWeather.RealFeelTemperature.Maximum.Unit);
+    app.feelsLikeMin.push(Math.round(dayWeather.RealFeelTemperature.Minimum.Value) + "° " + dayWeather.RealFeelTemperature.Minimum.Unit);
+    
     // send the weather forecast for the day to the GIPHY API
     app.retrieveGif(dayPhrase);
 
   })
 
-  
+
   // gets today's weekday as a numerical value
   const date = new Date();
   const today = date.getDay()
@@ -260,61 +260,61 @@ app.displayForecast = (arrayFromWeather) => {
     dateNameContainers[i].innerText = `${app.weekdays[today + i]}`
     dateContainers[i].innerText = `${app.date[i]}`;
     dayIconPhraseContainers[i].innerText = `Day: ${app.dayIconPhraseArray[i]}`
-
-    dayRainContainers[i].innerText = `Rain: ${app.dayRainArray[i]}`
-    daySnowContainers[i].innerText = `Snow: ${app.daySnowArray[i]}`
-    dayIceContainers[i].innerText = `Ice: ${app.dayIceArray[i]}`
-
-    dayWindContainers[i].innerText = `Wind: ${app.dayWindArray[i]}`
-
+      
+      dayRainContainers[i].innerText = `${app.dayRainArray[i]}`
+      daySnowContainers[i].innerText = `${app.daySnowArray[i]}`
+      dayIceContainers[i].innerText = `${app.dayIceArray[i]}`
+  
+    dayWindContainers[i].innerText = `${app.dayWindArray[i]}`
+    
     nightIconPhraseContainers[i].innerText = `Night: ${app.nightIconPhraseArray[i]}`
+    
+    nightRainContainers[i].innerText = `${app.nightRainArray[i]}`
+    nightSnowContainers[i].innerText = `${app.nightSnowArray[i]}`
+    nightIceContainers[i].innerText = `${app.nightIceArray[i]}`
+    nightWindContainers[i].innerText = `${app.nightWindArray[i]}`
 
-    nightRainContainers[i].innerText = `Rain: ${app.nightRainArray[i]}`
-    nightSnowContainers[i].innerText = `Snow: ${app.nightSnowArray[i]}`
-    nightIceContainers[i].innerText = `Ice: ${app.nightIceArray[i]}`
-    nightWindContainers[i].innerText = `Wind: ${app.nightWindArray[i]}`
-
-    tempMaxContainers[i].innerText = `High: ${app.realTempMax[i]}`
-    tempMinContainers[i].innerText = `Low: ${app.realTempMin[i]}`
-    feelMaxContainers[i].innerText = `Feels like high: ${app.feelsLikeMax[i]}`
-    feelMinContainers[i].innerText = `Feels like low: ${app.feelsLikeMin[i]}`
+    tempMaxContainers[i].innerText = `${app.realTempMax[i]} `
+    feelMaxContainers[i].innerText = `/ ${app.feelsLikeMax[i]}`
+    tempMinContainers[i].innerText = `${app.realTempMin[i]} `
+    feelMinContainers[i].innerText = `/ ${app.feelsLikeMin[i]}`
   }
 
 
 
   // Hiding the 0 precipitation divs
   dayRainContainers.forEach((day) => {
-    if (day.innerText == "Rain: 0mm") {
+    if(day.innerText == "0mm"){
       day.classList.add("hideWeather");
       day.setAttribute("aria-hidden", "true");
     }
   })
   daySnowContainers.forEach((day) => {
-    if (day.innerText == "Snow: 0cm") {
+    if(day.innerText == "0cm"){
       day.classList.add("hideWeather");
       day.setAttribute("aria-hidden", "true");
     }
   })
   dayIceContainers.forEach((day) => {
-    if (day.innerText == "Ice: 0mm") {
+    if(day.innerText == "0mm"){
       day.classList.add("hideWeather");
       day.setAttribute("aria-hidden", "true");
     }
   })
   nightRainContainers.forEach((day) => {
-    if (day.innerText == "Rain: 0mm") {
+    if(day.innerText == "0mm"){
       day.classList.add("hideWeather");
       day.setAttribute("aria-hidden", "true");
     }
   })
   nightSnowContainers.forEach((day) => {
-    if (day.innerText == "Snow: 0cm") {
+    if(day.innerText == "0cm"){
       day.classList.add("hideWeather");
       day.setAttribute("aria-hidden", "true");
     }
   })
   nightIceContainers.forEach((day) => {
-    if (day.innerText == "Ice: 0mm") {
+    if(day.innerText == "0mm"){
       day.setAttribute("aria-hidden", "true");
       day.classList.add("hideWeather");
     }
@@ -334,13 +334,13 @@ app.giphyUrl = "https://api.giphy.com/v1/gifs/search"
   // giphy api call
 app.retrieveGif = (iconPhrase) => {
 
-  console.log(iconPhrase)
   // creating the url
   const url = new URL(`${app.giphyUrl}`);
   url.search = new URLSearchParams({
     q: `weather+sky+nature+${iconPhrase}`,
     api_key: app.giphyApiKey
   });
+
 
   // fetching the data (gifs)
   fetch(url)
@@ -386,4 +386,4 @@ app.init = () => {
   app.showHide();
 }
 
-app.init();
+app.init(); 
